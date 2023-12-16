eeb57059bb60d94c1ff94c59d00fd720805b654c
Daniele Conti <fourlastor@users.noreply.github.com>
2023-12-16 11:29:01 +0100

    Remove physics related stuff

--- core/src/main/java/io/github/fourlastor/game/level/di/EcsModule.java
+++ core/src/main/java/io/github/fourlastor/game/level/di/EcsModule.java
@@ -4,9 +4,6 @@ import com.badlogic.ashley.core.ComponentMapper;
 import dagger.Module;
 import dagger.Provides;
 import io.github.fourlastor.game.di.ScreenScoped;
-import io.github.fourlastor.game.level.component.AnimatedImageComponent;
-import io.github.fourlastor.game.level.component.BodyBuilderComponent;
-import io.github.fourlastor.game.level.component.BodyComponent;
 import io.github.fourlastor.game.level.component.CityComponent;
 import io.github.fourlastor.game.level.component.EnemyComponent;
 import io.github.fourlastor.game.level.component.MovementComponent;
@@ -19,26 +16,8 @@ public class EcsModule {
 
     @Provides
     @ScreenScoped
-    public ComponentMapper<AnimatedImageComponent> animatedImageComponent() {
-        return ComponentMapper.getFor(AnimatedImageComponent.class);
-    }
-
-    @Provides
-    @ScreenScoped
     public ComponentMapper<ActorComponent> imageComponent() {
         return ComponentMapper.getFor(ActorComponent.class);
-    }
-
-    @Provides
-    @ScreenScoped
-    public ComponentMapper<BodyComponent> bodyComponent() {
-        return ComponentMapper.getFor(BodyComponent.class);
-    }
-
-    @Provides
-    @ScreenScoped
-    public ComponentMapper<BodyBuilderComponent> bodyBuilderComponent() {
-        return ComponentMapper.getFor(BodyBuilderComponent.class);
     }
 
     @Provides
