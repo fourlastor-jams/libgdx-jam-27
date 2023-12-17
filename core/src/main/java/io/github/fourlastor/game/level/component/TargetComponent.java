c89563f243b0b9235c0ec4e931b7bea84959b82c
Daniele Conti <fourlastor@users.noreply.github.com>
2023-12-17 12:00:41 +0100

    Ensure enemies don't spawn in unkillable positions

--- core/src/main/java/io/github/fourlastor/game/level/component/TargetComponent.java
+++ core/src/main/java/io/github/fourlastor/game/level/component/TargetComponent.java
@@ -8,8 +8,13 @@ public class TargetComponent implements Component {
     public final Vector2 hitTarget;
     public final Rectangle area;
 
-    public TargetComponent(Vector2 hitTarget) {
+    public final int minEnemyIndex;
+    public final int maxEnemyIndex;
+
+    public TargetComponent(Vector2 hitTarget, int minEnemyIndex, int maxEnemyIndex) {
         this.hitTarget = hitTarget;
         area = new Rectangle(hitTarget.x - 1, hitTarget.y - 1, 5, 5);
+        this.minEnemyIndex = minEnemyIndex;
+        this.maxEnemyIndex = maxEnemyIndex;
     }
 }
