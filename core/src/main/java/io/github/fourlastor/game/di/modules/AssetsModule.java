d249abb20f0169a8df730f9f59e16aa75064245a
Daniele Conti <fourlastor@users.noreply.github.com>
2023-12-17 11:28:58 +0100

    Import class

--- core/src/main/java/io/github/fourlastor/game/di/modules/AssetsModule.java
+++ core/src/main/java/io/github/fourlastor/game/di/modules/AssetsModule.java
@@ -2,6 +2,7 @@ package io.github.fourlastor.game.di.modules;
 
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
