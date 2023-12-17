2d8b6a4fb4ac4540db6f13852a4ca1fb9b99554f
Sandra Moen <sandramoen01@gmail.com>
2023-12-17 11:48:56 +0100

    added music + config

--- core/src/main/java/io/github/fourlastor/game/level/LevelScreen.java
+++ core/src/main/java/io/github/fourlastor/game/level/LevelScreen.java
@@ -3,8 +3,13 @@ package io.github.fourlastor.game.level;
 import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.utils.viewport.Viewport;
+
 import javax.inject.Inject;
+
+import io.github.fourlastor.game.SoundController;
 
 public class LevelScreen extends ScreenAdapter {
 
@@ -13,10 +18,13 @@ public class LevelScreen extends ScreenAdapter {
     private final EntitiesFactory entitiesFactory;
 
     @Inject
-    public LevelScreen(Engine engine, Viewport viewport, EntitiesFactory entitiesFactory) {
+    public LevelScreen(Engine engine, Viewport viewport, EntitiesFactory entitiesFactory, SoundController soundController, AssetManager assetManager) {
         this.engine = engine;
         this.viewport = viewport;
         this.entitiesFactory = entitiesFactory;
+
+        Music music = assetManager.get("audio/music/612631__szegvari__techno-retro-trance-sample-short-cinematic-120bpm-music-surround.ogg");
+        soundController.play(music, 1f, true);
     }
 
     @Override
